efed4fb5b551399f17526f96e5534470c2288a8d
ThePaladini <82978181+ThePaladini@users.noreply.github.com>
2021-09-27 14:25:06 -0300

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -33,7 +33,7 @@ class Servidor:
         id_conexao = (src_addr, src_port, dst_addr, dst_port)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
-            sequencia_serv = random.randint(0, 0xFFFF)
+            sequencia_serv = int.from_bytes(urandom(4), byteorder)
             ack_no = ack_no +  seq_no + 1
             segmento_serv = make_header(dst_port, src_port, sequencia_serv, ack_no, FLAGS_SYN | FLAGS_ACK)
             self.rede.enviar(fix_checksum(segmento_serv, dst_addr, src_addr), src_addr)
@@ -144,6 +144,7 @@ class Conexao:
         dados = fix_checksum(dados, self.id_conexao[0], self.id_conexao[2])
         self.servidor.rede.enviar(dados, self.id_conexao[2])
         self.concat.append(dados)
+        aux = len(dados)
         self.ultima_seq = self.ultima_seq + aux - 20
 
         if not self.t_ligado:
